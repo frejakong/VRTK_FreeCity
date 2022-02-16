a7e223e83e52c95aea89e15a1642501c076fead8
Kong Fanyu <freja.kong@outlook.com>
2022-02-16 17:35:12 +0800

    upload code
    all of the code, references and the description

---
+++ Assets/dialogue.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dialogue : MonoBehaviour
+{
+
+	//当碰撞开始时
+	void OnCollisionEnter(Collision other)
+	{
+		print("OnCollisionEnter");
+	}
+
+	//当碰撞的时候，一直持续到停止动作的时候
+	void OnCollisionStay(Collision other)
+	{
+		if (string.Equals(other.gameObject.name, "cub"))
+		{
+			print("OnCollisionStay");
+		}
+	}
+
+	//当碰撞结束后
+	void OnCollisionExit(Collision other)
+	{
+		print("OnCollisionExit");
+
+	}
+}
